e5910d00d427b6777d515ae3ab8365607f195c2f
Your Name <you@example.com>
2022-10-13 19:06:31 -0300

    classe conta criada

---
+++ src/banco/Conta.java
@@ -0,0 +1,16 @@
+package banco;
+
+public class Conta {
+    private int agencia;
+    private int numero;
+    private double saldo;
+    public int getAgencia() {
+        return agencia;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+}

6e3e4d671a3591a4ce45005d01d3b2da1708c368
ezin1 <eziofeitt15@gmail.com>
2022-11-02 15:06:06 -0300

    Cliente e endereço atualizado

--- src/banco/Cliente.java
+++ src/banco/Cliente.java
@@ -1,30 +1,56 @@
 package banco;
-
-import java.sql.Date;
+import java.util.List;
+// import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Cliente {
     private String nome;
     private String cpf;
     private LocalDate dataNascimento;
-    private Endereco endereco; 
+    private List<Endereco> enderecos = new ArrayList<>();
     
     public Cliente(String nome, String cpf, LocalDate dataNascimento, Endereco endereco){
         this.nome = nome;
         this.cpf = cpf;
         this.dataNascimento = dataNascimento;
-        this.endereco = endereco; 
+        this.enderecos.add(endereco);
     }
 
 
     public String getCpf() {
-        return cpf;
+        return cpf;   
     }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;   
+    }
+    
     public String getNome() {
-        return nome;
+        return nome;  
     }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public LocalDate getDataNascimento() {
         return dataNascimento;
     }
-
+    
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+    
+    public void addEndereco(Endereco endereco) {
+        this.enderecos.add(endereco);
+    }
+    
+    
+    public void removeEndereco(Endereco endereco) {
+        this.enderecos.remove(endereco);
+    }
 }

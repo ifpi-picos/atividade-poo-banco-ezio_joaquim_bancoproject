a287087260af3b34192d80dcf01ec3e8d78f582f
ezin1 <eziofeitt15@gmail.com>
2022-11-03 13:19:17 -0300

    Notificação criada

---
+++ src/banco/Notificacao.java
@@ -0,0 +1,10 @@
+package banco;
+
+public class Notificacao {
+    public void enviaEmail(String operacao, double valor) {
+        System.out.println("Enviando confirmação por email");
+        System.out.println("Operação desejada: " + operacao);
+        System.out.println("Valor da operação: " + valor);
+        System.out.println("Enviando confirmação por email");
+      }
+    }
